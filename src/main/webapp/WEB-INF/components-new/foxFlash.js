b1ceed9465f7355d6121020f74a1ed35e284211b
Nick Palmer <nick.palmer@fivium.co.uk>
2016-02-23 17:41:45 +0000

    FOXRD-918 - Keyboard access - Flashes
    Copied from Perforce
     Change: 106376

--- src/main/webapp/WEB-INF/components-new/foxFlash.js
+++ src/main/webapp/WEB-INF/components-new/foxFlash.js
@@ -1,4 +1,18 @@
-FOXflash = {
+/*
+ * FOX Flash
+ *
+ * Copyright 2016, Fivium Ltd.
+ * Released under the BSD 3-Clause license.
+ *
+ * Dependencies:
+ *   jQuery v1.11
+ */
+
+// Can be run through JSDoc (https://github.com/jsdoc3/jsdoc) and JSHint (http://www.jshint.com/)
+
+/*jshint laxcomma: true, laxbreak: true, strict: false */
+
+var FOXflash = {
 
   /**
    * Gets a jQuery pointing to the container div for flash messages, creating it if it doesn't exist.
@@ -7,7 +21,7 @@ FOXflash = {
    */
   _$getFlashContainer : function() {
     var container = $('.flash-message');
-    if(container.length == 0) {
+    if(container.length === 0) {
       container = $('<div class="flash-message"></div>').prependTo($(document.body));
       $('.flash-message').hide();
     }
@@ -22,13 +36,19 @@ FOXflash = {
    * @private
    */
   _insertFlashHTML : function(message, infoBoxClass) {
-
     var flashContainer = this._$getFlashContainer();
-    flashContainer.append('<div role="alert" class="info-box info-box-' + infoBoxClass + '">' +
-      '<div tabindex="0" class="flash-message-close icon-cross" title="Close this message" aria-label="Close this message">' +
-    '</div>' + message +'</div>');
-    flashContainer.find('.flash-message-close').click(function() { $(this).parent().fadeOut(100) });
+    flashContainer.append('<div role="alert" class="info-box info-box-' + infoBoxClass + '" tabindex="0">' +
+      '<button class="flash-message-close icon-cross" aria-label="Close this message">' +
+    '</button>' + message +'</div>');
+    flashContainer.find('.flash-message-close').click(function() { $(this).parent().fadeOut(100); });
     flashContainer.delay(200).fadeIn(300);
+    // The focus has to happen once the element is visible, 500ms should be when it's fully visible
+    setTimeout(function() {
+      if ($(document.activeElement).parents('.flash-message').length === 0) {
+        // Only focus the first child in the flash message container if the active element isn't already something in the flash message container
+        flashContainer.children().first().focus();
+      }
+    }, 500);
   },
 
   /**
@@ -37,7 +57,6 @@ FOXflash = {
    * @param {Object} flashProperties Additional properties such as style rules for the flash.
    */
   textFlash : function(message, flashProperties) {
-
     flashProperties = $.extend({
       displayType : 'info',
       cssClass: ''
