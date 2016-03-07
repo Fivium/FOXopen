53f42ce9ecc4fea1ceec20d0a39ec3c7f7a9c27c
Nick Palmer <nick.palmer@fivium.co.uk>
2016-03-07 14:05:11 +0000

    FOXRD-926 - Skiplinks - Missing tabindex on targets
    Copied from Perforce
     Change: 107188

--- src/main/java/net/foxopen/fox/module/serialiser/components/html/BufferComponentBuilder.java
+++ src/main/java/net/foxopen/fox/module/serialiser/components/html/BufferComponentBuilder.java
@@ -4,10 +4,16 @@ import net.foxopen.fox.XFUtil;
 import net.foxopen.fox.module.parsetree.evaluatedpresentationnode.EvaluatedBufferPresentationNode;
 import net.foxopen.fox.module.serialiser.SerialisationContext;
 import net.foxopen.fox.module.serialiser.components.ComponentBuilder;
+import net.foxopen.fox.module.serialiser.fragmentbuilder.MustacheFragmentBuilder;
 import net.foxopen.fox.module.serialiser.html.HTMLSerialiser;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class BufferComponentBuilder extends ComponentBuilder<HTMLSerialiser, EvaluatedBufferPresentationNode> {
+  private static final String SKIPLINK_TARGET_MUSTACHE_TEMPLATE = "html/SkipLinkTarget.mustache";
+
   private static final ComponentBuilder<HTMLSerialiser, EvaluatedBufferPresentationNode> INSTANCE = new BufferComponentBuilder();
 
   public static final ComponentBuilder<HTMLSerialiser, EvaluatedBufferPresentationNode> getInstance() {
@@ -23,13 +29,19 @@ public class BufferComponentBuilder extends ComponentBuilder<HTMLSerialiser, Eva
     if(pEvalBufferNode.getEvalClientVisibilityRule() != null) {
       //Wrapping div for contents hiding
       //TODO PN make this better (shouldn't really wrap arbitrary stuff in a div, might break the page)
-      pSerialiser.append("<div data-xfid=\"" + pEvalBufferNode.getSkipLinkID() + "\" class=\"" + pEvalBufferNode.getEvalClientVisibilityRule().getInitialCSSClass() + "\">");
+      pSerialiser.append("<div data-xfid=\"");
+      pSerialiser.append(pEvalBufferNode.getSkipLinkID());
+      pSerialiser.append("\" class=\"");
+      pSerialiser.append(pEvalBufferNode.getEvalClientVisibilityRule().getInitialCSSClass());
+      pSerialiser.append("\">");
     }
 
     if (!XFUtil.isNull(pEvalBufferNode.getRegionTitle())) {
-      pSerialiser.append("<a id=\"");
-      pSerialiser.append(pEvalBufferNode.getSkipLinkID());
-      pSerialiser.append("\" class=\"skipTarget\"></a>");
+      Map<String, Object> lTemplateVars = new HashMap<>(2);
+      lTemplateVars.put("SkipLinkID", pEvalBufferNode.getSkipLinkID());
+      lTemplateVars.put("RegionTitle", pEvalBufferNode.getRegionTitle());
+
+      MustacheFragmentBuilder.applyMapToTemplate(SKIPLINK_TARGET_MUSTACHE_TEMPLATE, lTemplateVars, pSerialiser.getWriter());
     }
 
     processChildren(pSerialisationContext, pSerialiser, pEvalBufferNode);
